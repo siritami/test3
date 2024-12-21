fecbebfdbbd82221288051ff7bf9a9ce8b35d1cb
siritami <103488094+siritami@users.noreply.github.com>
2024-12-22 00:41:57 +0700

    Update Revanced-Extended.sh

--- src/build/Revanced-Extended.sh
+++ src/build/Revanced-Extended.sh
@@ -35,40 +35,12 @@ revanced_dl(){
 	get_patches_key "youtube-revanced-extended"
 	get_apk "com.google.android.youtube" "youtube" "youtube" "google-inc/youtube/youtube"
 	patch "youtube" "revanced-extended" "inotia"
-	# Split architecture Youtube:
-	get_patches_key "youtube-revanced-extended"
-	for i in {0..3}; do
-		split_arch "youtube" "revanced-extended" "$(gen_rip_libs ${libs[i]})"
-	done
 }
 2() {
 	revanced_dl
-	# Patch YouTube Music Extended:
-	# Arm64-v8a
-	get_patches_key "youtube-music-revanced-extended"
-	get_apk "com.google.android.apps.youtube.music" "youtube-music-arm64-v8a" "youtube-music" "google-inc/youtube-music/youtube-music" "arm64-v8a"
-	patch "youtube-music-arm64-v8a" "revanced-extended" "inotia"
-	# Armeabi-v7a
-	get_patches_key "youtube-music-revanced-extended"
-	get_apk "com.google.android.apps.youtube.music" "youtube-music-armeabi-v7a" "youtube-music" "google-inc/youtube-music/youtube-music" "armeabi-v7a"
-	patch "youtube-music-armeabi-v7a" "revanced-extended" "inotia"
 }
 3() {
 	revanced_dl
-	# Patch Reddit:
-	get_patches_key "reddit-rve"
-	get_apk "com.reddit.frontpage" "reddit" "reddit" "redditinc/reddit/reddit" "Bundle"
-	patch "reddit" "revanced-extended"
-	#Disabled because lastest RVE patch youtube not have splits apk on APKMirror
-	#get_apk "com.google.android.youtube" "youtube-lite" "youtube" "google-inc/youtube/youtube" "Bundle_extract"
-	# Patch YouTube Lite Arm64-v8a:
-	#get_patches_key "youtube-revanced-extended"
-	#split_editor "youtube-lite" "youtube-lite-arm64-v8a" "include" "split_config.arm64_v8a split_config.en split_config.xhdpi split_config.xxxhdpi"
-	#patch "youtube-lite-arm64-v8a" "revanced-extended" "inotia"
-	# Patch YouTube Lite Armeabi-v7a:
-	#get_patches_key "youtube-revanced-extended"
-	#split_editor "youtube-lite" "youtube-lite-armeabi-v7a" "include" "split_config.armeabi_v7a split_config.en split_config.xhdpi split_config.xxxhdpi"
-	#patch "youtube-lite-armeabi-v7a" "revanced-extended" "inotia"
 }
 case "$1" in
     1)
